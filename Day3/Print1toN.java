bfc467bce6ac185523b7015f7ae03509ac629ac5
AnanyaSharma018 <ananyasharmaas018@gmail.com>
2026-06-15 22:37:18 +0530

    Add files via upload

---
+++ Day3/Print1toN.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Print1toN{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+        System.out.println("1 To "+ n);
+        for(int i =1;i<=n;i++){
+            System.out.println(i);
+        }
+        
+    }
+}
