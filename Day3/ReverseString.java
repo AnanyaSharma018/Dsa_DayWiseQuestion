648e59abf01f843a4f8785a4693530b043325e6e
AnanyaSharma018 <ananyasharmaas018@gmail.com>
2026-06-16 23:15:15 +0530

    Add files via upload

---
+++ Day3/ReverseString.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class ReverseString {
+    static String reverse(String str) {
+        if (str.isEmpty())
+            return str;
+        return reverse(str.substring(1)) + str.charAt(0);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        System.out.println("Reversed String: " + reverse(str));
+    }
+}
