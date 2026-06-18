fe13a8a4293941c7df5333f147f8e823ba52ad16
AnanyaSharma018 <ananyasharmaas018@gmail.com>
2026-06-18 22:43:01 +0530

    Add files via upload

---
+++ Day4/RemoveDuplicate.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class RemoveDuplicate{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size:");
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        System.out.println("Enter the elements:");
+        for(int i = 0;i<n;i++){
+            a[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            boolean duplicate = false;
+            for(int j = 0;j<i;j++){
+                if(a[i]==a[j]){
+                    duplicate = true;
+                    break;
+                }
+            }
+        
+        if(!duplicate)
+        System.out.print(a[i]+" " );
+        }
+    }
+}
