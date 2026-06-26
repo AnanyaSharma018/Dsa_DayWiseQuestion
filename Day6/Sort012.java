2378ec42f8cf5fa81da19ef4c71398ed6b133fcd
AnanyaSharma018 <ananyasharmaas018@gmail.com>
2026-06-26 23:32:34 +0530

    Add files via upload

---
+++ Day6/Sort012.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class Sort012 {
+    public static void sortArray(int[] arr) {
+        int low = 0, mid = 0, high = arr.length - 1;
+
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0:
+                    int temp = arr[low];
+                    arr[low] = arr[mid];
+                    arr[mid] = temp;
+                    low++;
+                    mid++;
+                    break;
+
+                case 1:
+                    mid++;
+                    break;
+
+                case 2:
+                    temp = arr[mid];
+                    arr[mid] = arr[high];
+                    arr[high] = temp;
+                    high--;
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 2, 1, 2, 0, 1, 0};
+        sortArray(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
